dec72f95c5d8bdc27f2d892a803af8c3dfe8bfc4
Kyle Dickey <kyledickey02@gmail.com>
2024-08-24 23:34:58 +0000

    init landing page

---
+++ apps/web/astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+    output: "server",
+    adapter: vercel(),
+    integrations: [
+        tailwind({
+            applyBaseStyles: true
+        }),
+        react()
+    ]
+});
