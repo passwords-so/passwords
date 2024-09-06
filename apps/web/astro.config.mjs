d58edeffa7ecd50b406bb8f55d16baa7df56d3c0
Kyle Dickey <kyledickey02@gmail.com>
2024-09-06 23:37:20 +0000

    docs

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -16,7 +16,6 @@ export default defineConfig({
     ],
     markdown: {
         shikiConfig: {
-            // https://shiki.style/themes
             theme: "dracula",
             wrap: true
         }
