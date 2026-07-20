d39eafe9d167ab5bd66748be9e57bf2976553686
Kyle Dickey <k@kyle.so>
2026-07-19 23:32:48 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 
 	"github.com/novmbrs/passwords/cmd"
 	"github.com/novmbrs/passwords/internal/config"
+	"github.com/novmbrs/passwords/internal/logger"
 )
 
 func main() {
+	os.Exit(run())
+}
+
+func run() int {
 	if err := config.LoadEnv(); err != nil {
-		os.Exit(1)
+		slog.Error("load environment", "error", err)
+		return 1
 	}
 
-	os.Exit(cmd.Execute())
+	rootDir, err := config.DefaultRootDir()
+	if err != nil {
+		slog.Error("find passwords directory", "error", err)
+		return 1
+	}
+
+	closeLogs, err := logger.Setup(rootDir)
+	if err != nil {
+		slog.Error("set up logging", "error", err)
+		return 1
+	}
+	defer closeLogs()
+
+	return cmd.Execute()
 }
